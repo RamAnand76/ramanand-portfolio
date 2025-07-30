08718a6baa4c6bc177f7828145a0684f8a9c982a
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-07-30 18:08:40 +0000

    Can you make the navbar visble always

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -34,7 +34,7 @@ export const FloatingNav = ({
         if (direction < 0) {
           setVisible(true);
         } else {
-          setVisible(false);
+          setVisible(true);
         }
       }
     }
